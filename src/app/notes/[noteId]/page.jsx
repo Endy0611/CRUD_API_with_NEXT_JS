f8dc668e00a73f112a5b18f1c56629880ad9eb65
Endy0611 <endyong18@gmail.com>
2026-03-15 18:21:38 +0700

    using fetch api to get user by id

--- src/app/notes/[noteId]/page.jsx
+++ src/app/notes/[noteId]/page.jsx
@@ -1,18 +1,28 @@
 import prisma from "@/lib/prisma";
 import React from "react";
 
-const page = async ( { params }) => {
-    const { noteId } = await params;
-  const note = await prisma.notes.findUnique({
-    where : {
-        id : Number(noteId) 
-    }
-  });
+const page = async ({ params }) => {
+  const { noteId } = await params;
+  //   const note = await prisma.notes.findUnique({
+  //     where: {
+  //       id: Number(noteId),
+  //     },
+  //   });
+
+  const res = await fetch(`http://localhost:3000/api/notes/${noteId}`);
+  const { data: note } = await res.json();
   return (
-    <div key={note.id}>
+    // <div key={note.id}>
+    //   <p>{note.content}</p>
+    //   <p>{note.createdAt?.toLocaleDateString()}</p>
+    //   <p>{note.updatedAt?.toLocaleDateString()}</p>
+    // </div>
+
+    <div>
+         <h1>Note #{note.id}</h1>
       <p>{note.content}</p>
-      <p>{note.createdAt?.toLocaleDateString()}</p>
-      <p>{note.updatedAt?.toLocaleDateString()}</p>
+      <p>Created: {new Date(note.createdAt).toLocaleDateString()}</p>
+      <p>Updated: {new Date(note.updatedAt).toLocaleDateString()}</p>
     </div>
   );
 };
